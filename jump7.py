be7f84202d7e48e0a2702034b5a97d8de176315a
shiyanlou-code <shiyanlou@code.com>
2021-07-19 20:03:47 +0800

    shiyaolou-code

---
+++ jump7.py
@@ -0,0 +1,25 @@
+#Demo
+
+print('hello world')
+
+i = 0
+
+for i in range(1,11):
+    if i != 6:
+        print(i)
+    else:
+        continue
+
+a = 21
+
+while a < 26:
+    print('the conut is ',a)
+    a +=1
+
+
+
+print('wrint something , bro')
+
+x = input()
+
+print("thanks for you '{}'".format(x))
